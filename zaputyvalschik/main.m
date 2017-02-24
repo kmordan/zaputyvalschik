8a14bf1a09f7fcec7ade099ad30001a00884b31a
Konstantin Mordan <konstantinmordan@gmail.com>
2017-02-24 17:50:33 +0300

    Create xcodeproj

---
+++ zaputyvalschik/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  zaputyvalschik
+//
+//  Created by Konstantin Mordan on 24/02/2017.
+//  Copyright © 2017 Konstantin Mordan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
