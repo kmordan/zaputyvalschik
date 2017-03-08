c3617133a852eeecb1dba21bf66623c0141c001e
Konstantin Mordan <konstantinmordan@gmail.com>
2017-03-08 13:06:37 +0300

    Added implementation for DeobfuscatedKeyFactory

--- zaputyvalschik/DeobfuscatedKeyFactory/DeobfuscatedKeyFactory.m
+++ zaputyvalschik/DeobfuscatedKeyFactory/DeobfuscatedKeyFactory.m
@@ -7,7 +7,28 @@
 //
 
 #import "DeobfuscatedKeyFactory.h"
+#import "Obfuscator.h"
 
 @implementation DeobfuscatedKeyFactory
+
++ (NSString *)deobfuscateKey:(unsigned char *)key
+                      ofSize:(unsigned long)size
+                   withSeeds:(NSArray<NSString *> *)seeds {
+    unsigned char *deobfuscatedKey = key;
+    unsigned char *deobfuscationTempResult = NULL;
+    
+    for (NSString *seed in seeds) {
+        deobfuscationTempResult = [Obfuscator mixKey:deobfuscatedKey
+                                              ofSize:size
+                                            withSeed:seed];
+        deobfuscatedKey = deobfuscationTempResult;
+        free(deobfuscationTempResult);
+    }
+    
+    NSString *result = [[NSString alloc] initWithBytes:deobfuscatedKey
+                                                length:size
+                                              encoding:NSUTF8StringEncoding];
+    return result;
+}
 
 @end
