46c6ff1462e2b2a014ef2c299c4b47ff0f56875e
Konstantin Mordan <konstantinmordan@gmail.com>
2017-03-25 12:50:34 +0300

    Added tests

--- zaputyvalschikUnitTests/DeobfuscatedKeyFactoryTests/DeobfuscatedKeyFactoryTests.m
+++ zaputyvalschikUnitTests/DeobfuscatedKeyFactoryTests/DeobfuscatedKeyFactoryTests.m
@@ -41,8 +41,47 @@
     NSString *deobfuscateKey = [DeobfuscatedKeyFactory deobfuscateKey:obfuscatedKey
                                                                ofSize:sizeof(obfuscatedKey)
                                                             withSeeds:seeds];
+    
     // then
     XCTAssertEqualObjects(expectedDeobfuscateKey, deobfuscateKey);
+}
+
+- (void)testDeobfuscateKeyOfSizeWithSeeds_returnsCorrectDeobfuscatedKeyWithOneSeed {
+    // given
+    const unsigned char obfuscatedKey[] = {0x1c, 0x98, 0x5d, 0x82, 0x30, 0x04, 0x7f, 0x8b,
+                                           0x74, 0x2a, 0xad, 0x08, 0xd4, 0x86, 0xe4, 0xb4,
+                                           0x52, 0x46, 0x1a, 0xe0, 0x41, 0xc9, 0x4d, 0xde,
+                                           0x72, 0x06, 0x7c, 0xd9, 0x74};
+    NSArray<NSString *> *seeds = @[@"первое зерно для ключика"];
+    
+    NSString *expectedDeobfuscateKey = @"74bmp13b083hdro4nf9wjer123000";
+    
+    // when
+    NSString *deobfuscateKey = [DeobfuscatedKeyFactory deobfuscateKey:obfuscatedKey
+                                                               ofSize:sizeof(obfuscatedKey)
+                                                            withSeeds:seeds];
+    
+    // then
+    XCTAssertEqualObjects(expectedDeobfuscateKey, deobfuscateKey);
+}
+
+- (void)testDeobfuscateKeyOfSizeWithSeeds_doesnotDeobfuscateKeyWithIncorrectSeeds {
+    // given
+    const unsigned char obfuscatedKey[] = {0x4d, 0x26, 0x8d, 0x0b, 0xec, 0xbc, 0xb2, 0x34, 0xf9,
+                                           0x54, 0x15, 0x3c, 0xd4, 0xda, 0x96, 0xb2, 0xaf, 0x28,
+                                           0x73, 0x7d, 0x7a, 0x2e, 0x97, 0x0d, 0xde, 0xe4, 0xff,
+                                           0x6b, 0x9d};
+    NSArray<NSString *> *seeds = @[@"NSASCIIStringEncoding", @"cStringUsingEncoding", @"new"];
+    
+    NSString *expectedDeobfuscateKey = @"dateWithTimeIntervalSince1970";
+    
+    // when
+    NSString *deobfuscateKey = [DeobfuscatedKeyFactory deobfuscateKey:obfuscatedKey
+                                                               ofSize:sizeof(obfuscatedKey)
+                                                            withSeeds:seeds];
+    
+    // then
+    XCTAssertNotEqualObjects(expectedDeobfuscateKey, deobfuscateKey);
 }
 
 @end
--- zaputyvalschikUnitTests/ObfuscatedKeyFactoryTests/ObfuscatedKeyFactoryTests.m
+++ zaputyvalschikUnitTests/ObfuscatedKeyFactoryTests/ObfuscatedKeyFactoryTests.m
@@ -50,4 +50,26 @@
     XCTAssertTrue(cmpResult == 0);
 }
 
+- (void)testObfuscateKeyWithSeeds_returnsCorrectObfuscatedKeyWithOneSeed {
+    // given
+    NSString *key = @"74bmp13b083hdro4nf9wjer123000";
+    NSArray<NSString *> *seeds = @[@"первое зерно для ключика"];
+    
+    const unsigned char expectedObfuscatedKey[] = {0x1c, 0x98, 0x5d, 0x82, 0x30, 0x04, 0x7f, 0x8b,
+                                                   0x74, 0x2a, 0xad, 0x08, 0xd4, 0x86, 0xe4, 0xb4,
+                                                   0x52, 0x46, 0x1a, 0xe0, 0x41, 0xc9, 0x4d, 0xde,
+                                                   0x72, 0x06, 0x7c, 0xd9, 0x74};
+    
+    // when
+    const unsigned char *obfuscatedKey = [ObfuscatedKeyFactory obfuscateKey:key
+                                                                  withSeeds:seeds];
+    
+    // then
+    int cmpResult = memcmp(expectedObfuscatedKey, obfuscatedKey, sizeof(expectedObfuscatedKey));
+    
+    free((unsigned char *)obfuscatedKey);
+    
+    XCTAssertTrue(cmpResult == 0);
+}
+
 @end
